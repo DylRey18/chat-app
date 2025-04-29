0889d4b918258a3e4a924514e9e3f5a9b5e22603
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-29 16:48:14 +0700

    finished routing frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -13,6 +14,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 //cookie parser must be made before the router
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+));
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
