3c7dd93962058e622bef8424f74724ce41524482
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-29 08:36:40 +0700

    created user signin

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userId, res) =>{
+    //make the token
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, 
+        {expiresIn:"7d"});
+    
+    //user cookie to be send to user
+    res.cookie("jwt",token, {
+        maxAge: 7*24*60*1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development"
+    })
+
+    return token;
+}
