a548767a548365a3438640817ed9af2d3ce0cc47
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2025-04-29 14:33:53 +0700

    finishing backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 //cookie parser must be made before the router
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 
 app.listen(PORT, () => {
